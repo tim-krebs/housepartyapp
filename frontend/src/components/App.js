0b1f4c4f349f2bb17d942296b64484f45a354813
Tim Krebs <timkrebs@Tims-MBP.fritz.box>
2021-04-15 20:41:59 +0200

    added function

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
-export default class App extends Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return <h1>Testing React</h1>;
-    }
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="center">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 const appDiv = document.getElementById("app");
