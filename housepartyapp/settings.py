c5ee6ab6ec6f20c454396c1939ef52e869dff6db
Tim Krebs <timkrebs@Tims-MBP.fritz.box>
2021-04-18 18:01:30 +0200

    v1.0.0

--- housepartyapp/settings.py
+++ housepartyapp/settings.py
@@ -13,20 +13,18 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
-import sys
-import dj_database_url
 
 
 
 #_____________________________________________________________________________
 # Original Django
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #_____________________________________________________________________________
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
@@ -37,20 +35,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 #SECURITY WARNING: keep the secret key used in production secret!
 
-#SECRET_KEY = '5a^#4sk&cu_0%tx28kqsi++tvg!6+=gav24c0ywx&+f2k2f)=@'
-#
-# #SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-#ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '192.168.1.35', '192.168.1.170']
+SECRET_KEY = '5a^#4sk&cu_0%tx28kqsi++tvg!6+=gav24c0ywx&+f2k2f)=@'
+
+ #SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '192.168.1.35', '192.168.1.170']
 #_____________________________________________________________________________
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+#SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 #
 ## SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+#DEBUG = os.getenv("DEBUG", "False") == "True"
 #
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+#ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 
@@ -107,29 +105,29 @@ WSGI_APPLICATION = 'housepartyapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 #_____________________________________________________________________________
 
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
-
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-    if os.getenv("DATABASE_URL", None) is None:
-        raise Exception("DATABASE_URL environment variable not defined")
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
+#DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+#
+#if DEVELOPMENT_MODE is True:
+#    DATABASES = {
+#        "default": {
+#            "ENGINE": "django.db.backends.sqlite3",
+#            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#        }
+#    }
+#elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+#    if os.getenv("DATABASE_URL", None) is None:
+#        raise Exception("DATABASE_URL environment variable not defined")
+#    DATABASES = {
+#        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+#    }
 
 
 
@@ -172,7 +170,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = ".frontend/static/"
+STATIC_URL = "/static/"
 #STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
